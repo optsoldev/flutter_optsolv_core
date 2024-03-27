2e967ae185481d0da8243af66c8c95e8e13bb880
Vladimir Christ <vladimir.christ@optsol.com.br>
2024-03-27 20:15:06 -0300

    Ajustes generics

--- lib/http/http-manager.dart
+++ lib/http/http-manager.dart
@@ -5,13 +5,16 @@ import 'package:dio/io.dart';
 import 'package:path/path.dart';
 
 import 'api-result.dart';
-import 'exceptions.dart';
 import 'base-model.dart';
+import 'exceptions.dart';
 import 'interceptors/interceptors.dart';
 
-typedef ObjectCreator<S> = S Function();
+typedef ObjectCreator<T> = T Function(Map<String, dynamic>);
 
 class HttpManager<T extends BaseModel> {
+  final ObjectCreator<T> creator;
+  late Dio _dio;
+
   HttpManager(this.creator) {
     _dio = Dio(BaseOptions(
       headers: {
@@ -21,63 +24,71 @@ class HttpManager<T extends BaseModel> {
       receiveTimeout: const Duration(seconds: 60),
     ));
 
-    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
-      client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    _dio.httpClientAdapter = IOHttpClientAdapter(createHttpClient: () {
+      var client = HttpClient();
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
       return client;
-    };
+    });
 
     _dio.interceptors.add(TokenInterceptor(dio: _dio));
     _dio.interceptors.add(RefreshTokenInterceptor(dio: _dio));
     _dio.interceptors.add(HandlerErrorInterceptor(dio: _dio));
     _dio.interceptors.add(ResponseInterceptor(dio: _dio));
-
-    this.model = this.creator();
   }
 
-  final ObjectCreator<T> creator;
-  late T model;
-
-  late Dio _dio;
-
-  @override
-  Future<Response<dynamic>> download(String url, String path, {Map<String, dynamic>? data, String method = 'GET'}) async {
-    var response = await _dio.download(url, path, data: data, options: Options(method: method));
-    return response;
-  }
-
-  @override
   Future<ApiResult<T>> delete(String url, Map<String, dynamic> data) async {
     var response = await _dio.delete(url, data: data);
     return _returnResponse(response);
   }
 
-  @override
+  Future<Response<dynamic>> download(String url, String path,
+      {Map<String, dynamic>? data, String method = 'GET'}) async {
+    var response = await _dio.download(url, path,
+        data: data, options: Options(method: method));
+    return response;
+  }
+
   Future<ApiResult<T>> get(String url) async {
     var response = await _dio.get(url);
     return _returnResponse(response);
   }
 
-  @override
+  Future<ApiResult<List<T>>> getAll(String url,
+      [Map<String, dynamic>? data]) async {
+    var response = await _dio.get(url, queryParameters: data);
+    return _returnResponseList(response);
+  }
+
   Future<dynamic> getDynamic(String url) async {
     var response = await _dio.get(url);
     return response;
   }
 
-  @override
-  Future<ApiResult<List<T>>> getAll(String url, [Map<String, dynamic>? data]) async {
-    var response = await _dio.get(url, queryParameters: data);
-    return _returnResponseList(response);
-  }
-
-  @override
-  Future<ApiResult<List<T>>> postAll(String url, [Map<String, dynamic>? data]) async {
-    var response = await _dio.post(url, data: data);
-    return _returnResponseList(response);
-  }
-
-  @override
   Future<ApiResult<T>> post(String url, Map<String, dynamic>? data) async {
     var response = await _dio.post(url, data: data);
+    return _returnResponse(response);
+  }
+
+  Future<ApiResult<List<T>>> postAll(String url,
+      [Map<String, dynamic>? data]) async {
+    var response = await _dio.post(url, data: data);
+    return _returnResponseList(response);
+  }
+
+  Future<ApiResult<T>> postAttachments(String url, Map<String, dynamic> data,
+      {String filePropertyName = "", List<File> files = const []}) async {
+    FormData fdata = FormData.fromMap(data);
+
+    for (final item in files) {
+      fdata.files.add(MapEntry(
+          filePropertyName,
+          await MultipartFile.fromFile(item.path,
+              filename: basename(item.path))));
+    }
+
+    var response = await _dio.post(url, data: fdata);
+
     return _returnResponse(response);
   }
 
@@ -86,31 +97,19 @@ class HttpManager<T extends BaseModel> {
     return response;
   }
 
-  Future<dynamic> putDynamic(String url, Map<String, dynamic>? data) async {
-    var response = await _dio.put(url, data: data);
-    return response;
-  }
-
-  @override
   Future<ApiResult<T>> put(String url, Map<String, dynamic>? data) async {
     var response = await _dio.put(url, data: data);
     return _returnResponse(response);
   }
 
-  Future<ApiResult<T>> postAttachments(String url, Map<String, dynamic> data, {String filePropertyName = "", List<File> files = const []}) async {
-    FormData fdata = FormData.fromMap(data);
-
-    for (final item in files) {
-      fdata.files.add(MapEntry(filePropertyName, await MultipartFile.fromFile(item.path, filename: basename(item.path))));
-    }
-
-    var response = await _dio.post(url, data: fdata);
-
-    return _returnResponse(response);
+  Future<dynamic> putDynamic(String url, Map<String, dynamic>? data) async {
+    var response = await _dio.put(url, data: data);
+    return response;
   }
 
   ApiResult<T> _returnResponse(Response<dynamic> response) {
-    if ((response.statusCode! >= 200 && response.statusCode! <= 299) || response.statusCode == 400) {
+    if ((response.statusCode! >= 200 && response.statusCode! <= 299) ||
+        response.statusCode == 400) {
       ApiResult<T> result = ApiResult<T>();
 
       result.success = response.data['success'];
@@ -118,7 +117,7 @@ class HttpManager<T extends BaseModel> {
       result.messages = response.data['messages'].toList().cast<String>();
 
       if (response.data['data'] != null) {
-        result.data = this.model.fromJson(response.data['data']);
+        result.data = this.creator(response.data['data']);
       }
 
       return result;
@@ -137,7 +136,8 @@ class HttpManager<T extends BaseModel> {
   }
 
   ApiResult<List<T>> _returnResponseList(Response<dynamic> response) {
-    if ((response.statusCode! >= 200 && response.statusCode! <= 299) || response.statusCode == 400) {
+    if ((response.statusCode! >= 200 && response.statusCode! <= 299) ||
+        response.statusCode == 400) {
       ApiResult<List<T>> result = ApiResult<List<T>>();
 
       result.success = response.data['success'];
@@ -148,9 +148,15 @@ class HttpManager<T extends BaseModel> {
       result.pageSize = response.data['pageSize'];
 
       if (response.data['data'] is List) {
-        result.data = response.data['data'].map((e) => this.model.fromJson(e)).toList().cast<T>();
+        result.data = response.data['data']
+            .map((e) => this.creator(e))
+            .toList()
+            .cast<T>();
       } else if (response.data['data']?['items'] != null) {
-        result.data = response.data['data']['items'].map((e) => this.model.fromJson(e)).toList().cast<T>();
+        result.data = response.data['data']['items']
+            .map((e) => this.creator(e))
+            .toList()
+            .cast<T>();
       } else {
         result.data = [];
       }
--- lib/services/base-service.dart
+++ lib/services/base-service.dart
@@ -2,11 +2,11 @@ import '../http/base-model.dart';
 import '../http/http-manager.dart';
 
 class BaseService<T extends BaseModel> {
-  late HttpManager manager;
+  late HttpManager<T> manager;
   late String baseUrl;
   final ObjectCreator<T> creator;
 
   BaseService(this.creator, this.baseUrl) {
-    manager = HttpManager(this.creator);
+    manager = HttpManager<T>(this.creator);
   }
 }
